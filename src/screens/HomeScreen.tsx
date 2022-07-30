e0b40f49ce1533fbf4252ec46c0c88dbc1ab358d
Leonardo Rodriguez <residenciasbigpink@girasolo.com>
2022-07-30 11:46:51 -0500

    configured stack navigation and ionicons

---
+++ src/screens/HomeScreen.tsx
@@ -0,0 +1,13 @@
+import { View } from 'react-native';
+import React from 'react';
+import Icon from 'react-native-vector-icons/Ionicons';
+
+const HomeScreen = () => {
+  return (
+    <View>
+      <Icon name="star-outline" size={50} color="black" />
+    </View>
+  );
+};
+
+export default HomeScreen;
